7c912e57753554d3cb6edcac6bab53f2f5506b53
Dylan Harness <dharness.engineer@gmail.com>
2023-05-04 17:43:24 -0700

    All pages, clown colored (#3)
    * .
    
    * .

--- src/pages/Propmaker.tsx
+++ src/pages/Propmaker.tsx
@@ -3,6 +3,16 @@ import { VimeoVideo } from "../components/VimeoVideo";
 import { PageContentWrapper } from "../components/PageContentWrapper";
 import { getProjectData, kebabToTitle } from "../utils";
 import { VideoTitle } from "../components/VideoTitle";
+import styled from "styled-components";
+import { FigureText } from "../components/FigureText";
+import { Video } from "../components/Video";
+import ruffs from "../assets/projects/propmaker/fanman-ruffs.mp4";
+
+const MediaSection = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-top: 80px;
+`;
 
 export function Propmaker() {
   const { vimeoId, name } = getProjectData("propmaker");
@@ -11,6 +21,15 @@ export function Propmaker() {
       <PageContentWrapper>
         <VimeoVideo vimeoId={vimeoId}></VimeoVideo>
         <VideoTitle>{kebabToTitle(name)}</VideoTitle>
+        <p>
+          This short walk-cycle was made entirely in Toon Boom, providing a
+          natural boil to the drawings. It depicts a prop maker going home after
+          a long day on the set.
+        </p>
+        <MediaSection>
+          <Video src={ruffs} width={"65%"} style={{ margin: "auto" }}></Video>
+          <FigureText>Fig 1. Frame by frame animation in Toonboom</FigureText>
+        </MediaSection>
       </PageContentWrapper>
     </>
   );
